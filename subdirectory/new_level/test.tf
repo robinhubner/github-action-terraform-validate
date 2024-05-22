2c690f6b842d32ed6e60881a9ac2da49dc0c8895
Robin Hübner <60304881+robinhubner@users.noreply.github.com>
2024-05-22 12:01:35 +0200

    Create test.tf

---
+++ subdirectory/new_level/test.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  # Intentional error: the key "bad_attribute" does not exist for the aws_instance resource
+  # bad_attribute = "invalid_value"
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.example.id
+}
